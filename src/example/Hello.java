57852afa509c7d85b6fa8bba7c5e4e9d3a237c87
joseph <jozek.kawalec@gmail.com>
2017-04-12 18:52:42 +0200

    part2

--- src/example/Hello.java
+++ src/example/Hello.java
@@ -9,6 +9,21 @@ public class Hello {
         System.out.println("standard message : " + message);
         System.out.println("capitalized by " + WordUtils.class.getName() 
                                 + " : " + WordUtils.capitalizeFully(message));
+        int monmax=0; 
+		int i,nb; 
+		try (CSVReader reader=new CSVReader(new FileReader("data.csv"))){ 
+			List<String[]> myEntries= reader.readAll(); 
+			for(i=0;i<myEntries.size();i++)
+			{ 
+				nb=Integer.parseInt(myEntries.get(i)[0]); 
+				System.out.println("nb lu : " + nb);
+				if(monmax<nb)
+				{ 
+					monmax=nb; 
+				} 
+			} 
+			System.out.println("Max = : " + monmax); 
+		} 
     }
 }
         
